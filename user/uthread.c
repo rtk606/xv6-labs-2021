3ef8951f5342bb17a28542479cfcecbda5f8d66a
ReleaseTheKraken <132792358+rtk606@users.noreply.github.com>
2023-10-11 22:41:30 +0100

    Completed lab-thread (A)

--- user/uthread.c
+++ user/uthread.c
@@ -10,14 +10,31 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context_thread {
+  uint64 ra; // return address
+  uint64 sp; // stack pointer
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct     context_thread context_thread;    /* Saved thread context */
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(uint64 old_context, uint64 new_context);
               
 void 
 thread_init(void)
@@ -62,6 +79,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)(&t->context_thread), (uint64)(&next_thread->context_thread));
   } else
     next_thread = 0;
 }
@@ -74,8 +92,14 @@ thread_create(void (*func)())
   for (t = all_thread; t < all_thread + MAX_THREAD; t++) {
     if (t->state == FREE) break;
   }
+  if(t >= all_thread + MAX_THREAD) {
+    printf("Error: no free thread to create\n");
+    exit(1);
+  }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->context_thread.sp = (uint64)t->stack + STACK_SIZE;
+  t->context_thread.ra = (uint64)func;
 }
 
 void 
